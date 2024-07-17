5816d47209a3d47fc28d24bd81df2434c000697c
Enrique Reverol <enrique@pop-os.lan>
2024-07-16 21:54:50 -0400

    exercise 1-5

--- temperature-translation.c
+++ temperature-translation.c
@@ -3,13 +3,13 @@
 void TemperatureConversion(char startingScale ,int max)
 {
     if(startingScale == 'f')
-        for (int i = 0; i <= max; i += 20) printf("%2dF\t%2dC\n", i, ((5) * (i-32) /9));
+        for (int i = max; i >= 0; i -= 20) printf("%2dF\t%2dC\n", i, ((5) * (i-32) /9));
     else if(startingScale == 'c')
-        for (int i = 0; i <= max; i += 20) printf("%2dC\t%2dF\n", i, ((i * (9/5)) + 32));
+        for (int i = max; i >= 0; i -= 20) printf("%2dC\t%2dF\n", i, ((i * (9/5)) + 32));
+
 }
 
 int main (void) {
-
     printf("Please state what temperature scale it'll transform from (f or c) :\n");
 
     char scale = getchar();
@@ -21,8 +21,7 @@ int main (void) {
         printf("\tCelsius to Fahrenheit\t");
     for(int i = 0; i < 10; i++) i == 9 ? printf("-\n") : putchar('-');
 
-    TemperatureConversion(scale,300);
-
+    TemperatureConversion(scale, 300);
 
     return 0;
 }
